27d0166a01a4587346e44d30139373041c5d4a39
Andrei Bostan <abostan.ir@gmail.com>
2020-05-27 00:51:01 +0200

    made some more fancy of a view

--- Client/src/view/SpecificRecipeController.java
+++ Client/src/view/SpecificRecipeController.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
@@ -11,6 +12,11 @@ import java.sql.SQLException;
 
 public class SpecificRecipeController extends ViewController
 {
+  @FXML public Label category;
+  @FXML public Label time;
+  @FXML public Label description;
+  @FXML public Label ingredients;
+  @FXML public Label directions;
   @FXML private Label recipeName;
 
   public SpecificRecipeController()
@@ -27,5 +33,16 @@ public class SpecificRecipeController extends ViewController
     recipeName.textProperty().bindBidirectional(
         super.getViewModels().getSpecificRecipeViewModel()
             .getRecipeNameProperty());
+    category.textProperty().bindBidirectional(super.getViewModels().getSpecificRecipeViewModel().getCategoryProperty());
+    time.textProperty().bindBidirectional(super.getViewModels().getSpecificRecipeViewModel().getTimeProperty());
+    description.textProperty().bindBidirectional(super.getViewModels().getSpecificRecipeViewModel().getDescriptionProperty());
+    ingredients.textProperty().bindBidirectional(super.getViewModels().getSpecificRecipeViewModel().getIngredientsProperty());
+    directions.textProperty().bindBidirectional(super.getViewModels().getSpecificRecipeViewModel().getDirectionsProperty());
+  }
+
+  public void goBack(ActionEvent actionEvent)
+      throws RemoteException, SQLException
+  {
+    super.getHandler().openView("AllRecipes");
   }
 }
--- Client/src/viewmodel/SpecificRecipeViewModel.java
+++ Client/src/viewmodel/SpecificRecipeViewModel.java
@@ -1,5 +1,7 @@
 package viewmodel;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import model.LocalModel;
@@ -13,21 +15,60 @@ public class SpecificRecipeViewModel
   private LocalModel model;
   private Recipe recipe;
   private StringProperty recipeName;
+  private StringProperty category;
+  private StringProperty time;
+  private StringProperty description;
+  private StringProperty ingredients;
+  private StringProperty directions;
 
   public SpecificRecipeViewModel(LocalModel model)
   {
     this.model = model;
     this.recipeName = new SimpleStringProperty();
+    this.category = new SimpleStringProperty();
+    this.time = new SimpleStringProperty();
+    this.description = new SimpleStringProperty();
+    this.ingredients = new SimpleStringProperty();
+    this.directions = new SimpleStringProperty();
   }
 
   public void setRecipe(int id) throws RemoteException, SQLException
   {
     this.recipe = model.getRecipes().getRecipeById(id);
     recipeName.setValue(recipe.getRecipeName());
+    category.setValue(recipe.getCategory());
+    time.setValue(Integer.toString(recipe.getPreparationTime()));
+    description.setValue(recipe.getDescription());
+    directions.setValue(recipe.getDescription());
   }
 
   public StringProperty getRecipeNameProperty()
   {
     return recipeName;
+  }
+
+  public StringProperty getCategoryProperty()
+  {
+    return category;
+  }
+
+  public StringProperty getTimeProperty()
+  {
+    return time;
+  }
+
+  public StringProperty getDescriptionProperty()
+  {
+    return description;
+  }
+
+  public StringProperty getIngredientsProperty()
+  {
+    return ingredients;
+  }
+
+  public StringProperty getDirectionsProperty()
+  {
+    return directions;
   }
 }
