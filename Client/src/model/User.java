acdec3b57d13ac60ed6d58a035fe6cc41e55d025
EdvinasAndrijauskas <edvinas.andrijauskas2000@gmail.com>
2020-05-30 15:37:52 +0200

    added java comments for recipe/recipeList/User

--- Client/src/model/User.java
+++ Client/src/model/User.java
@@ -3,17 +3,34 @@ package model;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * A class representing  a user
+ *
+ * @author Edvinas Andrijauskas
+ * @version 1.0 - May 2020
+ */
+
 public class User
 {
   private int ID;
   private static User instance;
   private static Lock lock = new ReentrantLock();
 
+  /**
+   * One-argument constructor
+   *
+   * @param ID user id
+   */
   private User(int ID)
   {
     this.ID = ID;
   }
 
+  /**
+   * Getter for singleton instance
+   *
+   * @param ID we are getting a User with set id
+   */
   public static void getInstance(int ID)
   {
     if (instance == null)
@@ -28,6 +45,11 @@ public class User
     }
   }
 
+  /**
+   * Getter for singleton instance
+   *
+   * @return getting user
+   */
   public static User getInstance()
   {
     synchronized (lock)
@@ -36,6 +58,11 @@ public class User
     }
   }
 
+  /**
+   * Getter for user id
+   *
+   * @return Id as an integer
+   */
   public int getUserID()
   {
     return ID;
